0815af6b0e7822d304347dcc4a4aa61dd22d91bb
Posi-10 <64515605+Posi-10@users.noreply.github.com>
2022-03-29 10:14:23 -0600

    Capitulo 2
    Uso de extends, section e yield en el layout

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layout/plantilla')
+
+@section('tituloPagina', 'Crud con laravel 8')
+
+@section('contenido')
+    <div class="row">
+        <div class="col">
+            <h1>Hola usando layouts</h1>
+            <h2><i class="fa-solid fa-cannabis"></i></h2>
+        </div>
+    </div>
+@endsection
